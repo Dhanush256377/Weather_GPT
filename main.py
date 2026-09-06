a0ff86c0e2f8fac297b6dbd58d2d4ce5f2a82c8c
Dhanush256377 <71382507010.dhanush@sritcbe.ac.in>
2026-09-06 09:49:50 +0530

    allow Netlify frontend

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
+        "https://weather-india-gpt.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
